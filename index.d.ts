923e0dea218f0bad068af8e274dee844f559539e
z.wieczorek <zbyszek.wieczorek@gmail.com>
2016-09-29 15:55:39 +0200

    typescript

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
-import { DfiObject } from "./src/dfiObject";
-import { DfiModel } from './src/dfiModel';
-import { DfiCollection } from './src/dfiCollection';
+import DfiObject from "./src/dfiObject";
+import DfiModel from "./src/dfiModel";
+import DfiCollection from "./src/dfiCollection";
 export { DfiObject };
 export { DfiModel };
 export { DfiCollection };
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 "use strict";
 const dfiObject_1 = require("./src/dfiObject");
-exports.DfiObject = dfiObject_1.DfiObject;
-const dfiModel_1 = require('./src/dfiModel');
-exports.DfiModel = dfiModel_1.DfiModel;
-const dfiCollection_1 = require('./src/dfiCollection');
-exports.DfiCollection = dfiCollection_1.DfiCollection;
+exports.DfiObject = dfiObject_1.default;
+const dfiModel_1 = require("./src/dfiModel");
+exports.DfiModel = dfiModel_1.default;
+const dfiCollection_1 = require("./src/dfiCollection");
+exports.DfiCollection = dfiCollection_1.default;
 //# sourceMappingURL=index.js.map
