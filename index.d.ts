506bdd1b4e2858331ba46a64617d5e8b87be4c46
z.wieczorek <zbyszek.wieczorek@gmail.com>
2016-10-28 09:52:25 +0200

    definition fix

--- index.d.ts
+++ index.d.ts
@@ -11,7 +11,7 @@ declare module "local-dfi-base" {
     }
     export interface IDfiBaseCollectionConfig<M> extends IDfiBaseObjectConfig {
         idField?: string;
-        model?: M;
+        model?: typeof M;
     }
 
     // events
@@ -52,11 +52,17 @@ declare module "local-dfi-base" {
      */
     class EventEmitter {
         public eventNames(exists?: boolean): Array<TEventName>;
+
         public listeners(event?: TEventName, exists?: boolean): Function[] | boolean;
+
         public emit(event: TEventName, a1: any, a2: any, a3: any, a4: any, a5: any, ...args: any[]): boolean;
+
         public on(event: TEventName, fn: Function, context?: any): this;
+
         public once(event: TEventName, fn: Function, context?: any): EventEmitter;
+
         public removeListener(event: TEventName, fn?: Function, context?: any, once?: boolean): this;
+
         public removeAllListeners(event?: TEventName): this;
     }
 
@@ -64,12 +70,19 @@ declare module "local-dfi-base" {
     export abstract class DfiObject {
         public destroyed?: boolean;
         public readonly logger: DebugLogger;
+
         constructor(options?: IDfiBaseObjectConfig);
+
         public destroy(): void;
+
         public toPlain(): Object;
+
         protected getProp(key: any): any;
+
         protected setProp(key: any, value: any): DfiObject;
+
         protected hasProp(key: any): boolean;
+
         protected removeProp(key: any): boolean;
     }
 
@@ -78,12 +91,19 @@ declare module "local-dfi-base" {
     export abstract class DfiEventObject extends DfiObject {
         public static readonly events: IDfiBaseEventObjectEvents;
         public readonly eventNames: (string | symbol)[];
+
         constructor(options?: IDfiBaseObjectConfig);
+
         public destroy(): void;
+
         public on(event: TEventName, fn: Function, context?: any): EventEmitter;
+
         public once(event: TEventName, fn: Function, context?: any): EventEmitter;
+
         public emit(event: TEventName, ...args: any[]): boolean;
+
         public off(event: TEventName, fn?: Function, context?: any, once?: boolean): EventEmitter;
+
         public removeAllListeners(event?: string): EventEmitter;
     }
 
@@ -93,13 +113,21 @@ declare module "local-dfi-base" {
         protected static map: Map<string, string>;
         public readonly id: any;
         public readonly lastUpdate: number;
+
         constructor(attributes?: IDfiBaseModelAttribs, options?: IDfiBaseModelConfig);
+
         public destroy(): void;
+
         public toJSON(): Object;
+
         public toPlain(): Object;
+
         protected get(attribute: string): any;
+
         protected has(attribute: string): boolean;
+
         protected set(attribute: string | Object, value: any, silent?: boolean): this;
+
         protected remove(attribute: any): any;
     }
 
@@ -107,26 +135,42 @@ declare module "local-dfi-base" {
     export abstract class DfiCollection<M extends DfiModel> extends DfiEventObject {
         public static readonly events: IDfiBaseCollectionEvents;
         public readonly size: number;
+
         constructor(options?: IDfiBaseCollectionConfig<M>);
+
         public destroy(): void;
+
         public toJSON(): Object;
+
         protected has(element: M | any): boolean;
+
         protected get(id: any): M;
+
         protected add(element: M): this;
+
         protected remove(element: M | any): boolean;
+
         protected keys(): Array<any>;
+
         protected clear(): this;
+
         protected forEach<K, V>(fn: (value: V, index: K, map: Map<K, V>) => void, thisArg?: any): void;
+
         protected toArray(): Array<M>;
+
         protected proxyOn(event: TEventName, fn: Function, context?: any): this;
+
         protected proxyOff(event: TEventName, fn: Function, context?: any): this;
+
         protected proxyOffAll(): this;
     }
 
     // utils
     export class DfiUtil {
         public static maybeCallbackOnce(fn: IDfiCallbackResult, context: any, err?: any, response?: any): void;
+
         public static maybeCallback(fn: IDfiCallbackResult, context: any, err?: any, response?: any): void;
+
         public static obj2map<V>(obj: {
             [key: string]: V;
         }): Map<string, V>;
