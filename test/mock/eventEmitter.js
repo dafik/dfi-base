37ff16aadb1d5e2c993294c5643120abe87e9029
Zbyszek Wieczorek <zbyszek.wieczorek@gmail.com>
2018-01-08 08:57:02 +0100

    remove js and map from repo

--- test/mock/eventEmitter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dfiEventEmitter_1 = require("../../src/dfiEventEmitter");
-class TestEventEmitter extends dfiEventEmitter_1.EventEmitter {
-}
-exports.default = TestEventEmitter;
-//# sourceMappingURL=eventEmitter.js.map
--- test/mock/eventObject.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dfiEventObject_1 = require("../../src/dfiEventObject");
-class TestEventObject extends dfiEventObject_1.default {
-}
-exports.default = TestEventObject;
-//# sourceMappingURL=eventObject.js.map
